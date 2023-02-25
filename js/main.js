9641faaccb1c8e0a0600bc646d3cfcf94ba91420
Lucas Lazarini <lucasalmeidalazarini@gmail.com>
2023-02-25 17:16:10 -0300

    services section

--- js/main.js
+++ js/main.js
@@ -4,7 +4,22 @@ const Botoes = document.querySelectorAll('.side__menu-button')
 for (let i = 0; i < Botoes.length; i++) {
     const Botao = Botoes[i]
     Botao.onclick = function () {
-        MenuLateral.classList.toggle('hide-menu')
+        MenuLateral.classList.toggle('hide')
     }
-}
+};
 
+const Services = document.querySelectorAll('.service-item')
+const ServicesTitle = document.querySelectorAll('.service-title'); 
+const ServicesDesc = document.querySelectorAll('.service-desc');
+
+for (let i = 0; i < Services.length; i++) {
+    const Service = Services[i]
+    const ServiceTitle = ServicesTitle[i]
+    const ServiceDesc = ServicesDesc[i]
+    const effects = ['teste']
+    for (let j = 0; j < effects.length; j++) {
+        ServiceTitle.addEventListener('click', function () {
+            ServiceDesc.classList.toggle('hide-service')
+            Service.classList.toggle(effects[j])
+    })
+}}
