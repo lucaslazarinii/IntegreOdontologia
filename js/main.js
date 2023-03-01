391e6a7453f07b1e5e9c2a2161294b6c5cd2dc41
Lucas Lazarini <lucasalmeidalazarini@gmail.com>
2023-03-01 19:05:43 -0300

    services finished

--- js/main.js
+++ js/main.js
@@ -23,3 +23,18 @@ for (let i = 0; i < Services.length; i++) {
             Service.classList.toggle(effects[j])
     })
 }}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = 300 + "px";
+    }
+  });
+}
